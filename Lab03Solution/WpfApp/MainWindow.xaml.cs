f932cb5c22862b36fd9e96c443bfc7dcfc57b2d6
Tomasz Mikuczewski <miktomi15@gmail.com>
2018-02-13 11:01:27 +0100

    Laboratorium (03) 2017-12-05 Dodano: - treść zadań dla laboratorium 03, - zadania 01 oraz 04. Zmieniono: - LICENSE, - konfigurację serwera budowania.

---
+++ Lab03Solution/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+using Res = WpfApp.Properties.Resources;
+
+namespace WpfApp
+{
+	public partial class MainWindow
+	{
+		static MainWindow()
+		{
+			CultureInfo currentCulture = CultureInfo.CurrentCulture;
+			Thread.CurrentThread.CurrentCulture = currentCulture;
+			Thread.CurrentThread.CurrentUICulture = currentCulture;
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			if (MessageBox.Show(Res.CloseConfirmation, Res.Exit, MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+			{
+				base.OnClosing(e);
+			}
+			else
+			{
+				e.Cancel = true;
+			}
+		}
+
+		private void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			switch (((MenuItem) e.Source).Name)
+			{
+				case "Exit":
+					Close();
+					break;
+				default:
+					return;
+			}
+		}
+	}
+}
